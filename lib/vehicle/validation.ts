94eb38916952f6b468bb726dcf8e2606e0fa0a7b
John Carlo Miel <johnmiel@skydev.solutions>
2022-11-08 20:57:52 +0800

    Update validation

--- lib/vehicle/validation.ts
+++ lib/vehicle/validation.ts
@@ -1,13 +1,13 @@
 import Joi from "joi";
 
 export const VehicleSchema = Joi.object({
-    make: Joi.string().required(),
-    model: Joi.string().required(),
+    make: Joi.string().required().max(250),
+    model: Joi.string().required().max(250),
     year: Joi.number().required().min(1900).max(new Date().getFullYear()),
 })
 
 export const VehicleGetQueryParamsSchema = Joi.object({
-    make: Joi.string(),
-    model: Joi.string(),
+    make: Joi.string().max(250),
+    model: Joi.string().max(250),
     year: Joi.number().min(1900).max(new Date().getFullYear()),
 })
